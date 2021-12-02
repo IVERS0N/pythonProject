0d49d77c3ee1a8bde93a7f6851133d9a78faf1ec
IVERS0N <amatira@institutmvm.cat>
2021-12-02 15:53:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+        mi_variable = 3
+        print(mi_variable)
+
