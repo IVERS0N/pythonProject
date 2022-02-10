fad101063f00f107500c0f02b53c906a12769cf3
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-02-10 16:22:16 +0100

    Update main.py

--- exercici3/main.py
+++ exercici3/main.py
@@ -1,7 +1,11 @@
-
 def decimaltobinary(num):
   if num > 1:
     decimaltobinary(num // 2)
-  print(num % 2, end='')
+  print(num % 2, end="")
 numero=int(input("Entra un numero: "))
-decimaltobinary(numero)
+
+def main():
+    decimaltobinary(numero)
+
+if __name__ == "__main__":
+    main()
