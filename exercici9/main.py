460e265d533940393e6646c35f5dc47e032e4033
IVERS0N <95426224+IVERS0N@users.noreply.github.com>
2022-02-11 15:21:29 +0100

    Update main.py

--- exercici9/main.py
+++ exercici9/main.py
@@ -1,1 +1,14 @@
 
+
+binary = input("Enter a binary code: ")
+
+def binarytodecimal():
+    decimal = 0
+    for i in binary:
+        decimal = decimal * 2 + int(i)
+        print(decimal)
+def main():
+    binarytodecimal()
+
+if __name__ == "__main__":
+    main()
